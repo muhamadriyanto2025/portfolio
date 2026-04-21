63a3b94fa7b9954a6504632df1e88a9f8e88df2b
Muhamad Riyanto <muhamad.riyanto2025@gmail.com>
2026-04-21 21:21:34 +0900

    web portfolio 1.0

---
+++ dist/img/js/script.js
@@ -0,0 +1,19 @@
+// hamburger
+const hamburger = document.getElementById('hamburger');
+
+ hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('hamburger-active');
+});
+
+// navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }               
+};
+console.log(header);
