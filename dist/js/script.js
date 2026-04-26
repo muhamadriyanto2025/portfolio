4c1771c4bd52267c6609e41006b9466dd8d803d8
Muhamad Riyanto <muhamad.riyanto2025@gmail.com>
2026-04-26 19:33:38 +0900

    menambahkan fitur dark mode responsive mobile, dan desktop all

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,37 @@ window.onscroll = function () {
         header.classList.remove('navbar-fixed');
     }               
 };
+
+// dark mode toggle
+document.addEventListener('DOMContentLoaded', function () {
+  const toggle = document.getElementById('theme-toggle');
+  const html = document.documentElement;
+
+  const iconDark = document.getElementById('icon-dark');
+  const iconLight = document.getElementById('icon-light');
+
+  if (!toggle) return; // biar ga error
+
+  // load awal
+  if (localStorage.getItem('theme') === 'dark') {
+    html.classList.add('dark');
+    iconDark.classList.add('hidden');
+    iconLight.classList.remove('hidden');
+  }
+
+  toggle.addEventListener('click', () => {
+    console.log('klik detected'); // debug
+
+    html.classList.toggle('dark');
+
+    if (html.classList.contains('dark')) {
+      localStorage.setItem('theme', 'dark');
+      iconDark.classList.add('hidden');
+      iconLight.classList.remove('hidden');
+    } else {
+      localStorage.setItem('theme', 'light');
+      iconDark.classList.remove('hidden');
+      iconLight.classList.add('hidden');
+    }
+  });
+});
